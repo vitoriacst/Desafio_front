2d202e5d52d28feb30d7084740adc47b891533bb
vitoriacst <vitoria.cst.holanda@gmail.com>
2024-07-05 18:55:44 -0300

    feat: :lipstick: construindo e estilizando o card de eventos

--- app/src/components/EventCard.tsx
+++ app/src/components/EventCard.tsx
@@ -14,8 +14,8 @@ export default function EventComponent({ eventData }: EventProps) {
     <div className="gap-6 pt-4 p-2">
       <div className="flex flex-col gap-6 justify-center mt-12 md:flex-row">
         {data && data.map((event) => (
-          <div key={event.id} className="p-4 flex flex-col rounded-sm bg-soft-blue">
-            <h3 className="mb-2 text-2xl font-bold text-blocky-dark">{event.name}</h3>
+          <div key={event.id} className="p-4 flex flex-col rounded-sm bg-gray w-3/5">
+            <h3 className="mb-2 text-2xl font-bold text-black">{event.name}</h3>
             <span>{formatDate(event.start_date)}</span>
             {event.status === false ? (
               <div className="flex items-center p-4 mb-4  mt-4 text-sm text-blocky-dark rounded-lg bg-gray" role="alert">
@@ -28,15 +28,10 @@ export default function EventComponent({ eventData }: EventProps) {
                 </div>
               </div>
             ): (
-              <div className="flex items-center p-4 mb-4  mt-4 text-sm text-blocky-dark rounded-lg bg-gray" role="alert">
-              <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-              </svg>
-              <span className="sr-only">Info</span>
-              <div>
-                <span className="font-medium">Restam alguns ingressos</span>
+              <div className="bg-soft-blue border-t border-b border-soft-green text-blocky-dark px-4 py-3 mt-4 mb-4 rounded-sm" role="alert">
+                <p className="font-bold">Importante</p>
+                <p className="text-sm">Restam alguns ingressos.</p>
               </div>
-            </div>
             )
             }
             <button className="bg-dark-blue p-2 rounded-md disabled:opacity-20" disabled={!event.status}>
