9439177c0645c4abb4f4a823e2abed433dd331cb
vitoriacst <vitoria.cst.holanda@gmail.com>
2024-07-07 23:54:39 -0300

    fix: :fire: removendo o redux do cart

--- app/src/redux/actions/cart.ts
+++
@@ -1,7 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-interface MeuObjeto {
-  [chave: string]: number;
-}
-
-export const carts = createAction<MeuObjeto>('updateCartQuantities/carts');
--- app/src/redux/reducers/index.ts
+++ app/src/redux/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import CartSlice from "./CartSlice";
 import EventsSlice from "./EventsSlice";
 
 const rootReducer = combineReducers({
   EventsSlice,
-  CartSlice
 })
 
 export type RootState = ReturnType<typeof rootReducer>
